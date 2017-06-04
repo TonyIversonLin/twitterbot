267cd96b1f3fe6a5fb62265d272c42f6019ff820
Tony Lin <tonyiversonlin@gmail.com>
2017-06-04 12:28:24 -0400

    demo get and post tweet

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 console.log('Twitter Bot Activate!');
+
 let Twit = require('twit');
 let config = require('./config.js');
 
 let T = new Twit(config);
+
+T.get('search/tweets', { q: 'messi since:2017-06-01', count: 10 }, function(err, data, response) {
+  let tweets = data.statuses.map(message => message.text);
+  console.log(tweets);
+});
+
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  let result = err || data || response;
+	console.log(result);
+})
