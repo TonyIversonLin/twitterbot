d30b035433d8876270ad9c43a0948e5cc67f36b9
Tony Lin <tonyiversonlin@gmail.com>
2017-06-04 16:37:43 -0400

    tweet image

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 console.log('Twitter Bot Activate!');
 
-let Twit = require('twit');
-let config = require('./config.js');
+const Twit = require('twit');
+const config = require('./config.js');
+const fs = require('fs'); 
 
 let T = new Twit(config);
 
-T.get('search/tweets', { q: 'messi since:2017-06-01', count: 10 }, function(err, data, response) {
-  let tweets = data.statuses.map(message => message.text);
-  console.log(tweets);
-});
+//get and post tweet --------------------------------------------------------
+function getTweet(keyword) {
+	T.get('search/tweets', { q: `${keyword} since:2017-06-01`, count: 10 }, function(err, data, response) {
+	  let tweets = data.statuses.map(message => message.text);
+	  console.log(tweets);
+	});	
+}
+//getTweet('messi');
 
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  let result = err || data || response;
-	console.log(result);
-})
+function postTweet(tweet) {
+	T.post('statuses/update', tweet, function(err, data, response) {
+	  err ? console.log('something went wrong') : console.log('operation success')
+	}); 
+}
+//postTweet({status: 'hello world!'});
+
+
+//Tweet in a time intervel -----------------------------------
+function randomTweet() {
+	let randomMessage = Math.floor(Math.random() * 100);
+	let tweet = {
+		status: 'A lucky number: ' + randomMessage + ' from node.js app'
+	}
+	postTweet(tweet);
+}
+//randomTweet();
+//setInterval(randomTweet, 1000 * 10);
+
+
+// tweet image-------------------------------------------------------------------------------------
+let imagePath = './img/coding2.jpg';
+function imageTweet(image) {
+	let b64content = fs.readFileSync(image, { encoding: 'base64' });
+
+	T.post('media/upload', {media_data: b64content}, (err, data, response) => {
+  	let meta_params = { media_ids: data.media_id_string, status: "image from node.js app" };
+  	postTweet(meta_params);
+	})
+}
+imageTweet(imagePath);
+
+
+// tweetbot reply----------------------------------------------------------------------------------
